35a42032adc97a002b2d6584a9f65d2970ccfd47
Alvaro <alvaro.g.a.99@gmail.com>
2020-11-13 09:28:55 +0100

    update

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,17 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+import java.awt.*;
+
 public class Controller {
+    @FXML
+    private TextField Text1;
+    @FXML
+    private TextField Text2;
+
+    private Label Label1;
+
+    private Label Label2;
 }
