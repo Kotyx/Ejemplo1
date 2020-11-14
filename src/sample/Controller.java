876361b58c6bbe33993803b588dc36ca33715be4
Alvaro <alvaro.g.a.99@gmail.com>
2020-11-14 18:52:44 +0100

    update

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,31 +2,26 @@ package sample;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.PasswordField;
-import javafx.scene.text.Font;
+
 import javafx.scene.control.TextField;
-import javafx.scene.control.Button;
+
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 
 public class Controller {
 
     String contraseña="";
-    //@FXML
-    //private TextField Text1;
+
     @FXML
     private TextField Text2;
     @FXML
     private PasswordField Passw1;
-    //@FXML
-    //private Label Label1;
-    //@FXML
-    //private Label Label2;
+
     @FXML
     private Label Label3;
     @FXML
     private Label LVerContraseña;
-   // @FXML
-    //private Button bGuardar;
+
 
     @FXML
     public void onbGuardarClicked(MouseEvent event){
