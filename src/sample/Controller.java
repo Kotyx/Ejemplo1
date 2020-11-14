a0da75e8ef865c353404c957401dc1813b7ceceb
Alvaro <alvaro.g.a.99@gmail.com>
2020-11-14 13:14:02 +0100

    update

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,17 +1,46 @@
 package sample;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.text.Font;
 import javafx.scene.control.TextField;
-
-import java.awt.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 
 public class Controller {
-    @FXML
-    private TextField Text1;
+
+    String contraseña="";
+    //@FXML
+    //private TextField Text1;
     @FXML
     private TextField Text2;
+    @FXML
+    private PasswordField Passw1;
+    //@FXML
+    //private Label Label1;
+    //@FXML
+    //private Label Label2;
+    @FXML
+    private Label Label3;
+    //@FXML
+    //private Label LVerContraseña;
+   // @FXML
+    //private Button bGuardar;
 
-    private Label Label1;
+    @FXML
+    public void onbGuardarClicked(MouseEvent event){
+       Label3.setVisible(true);
+    }
 
-    private Label Label2;
+    @FXML
+    public void onLVerContraseñaFocus(){
+        contraseña=Passw1.getText();
+        Text2.setText(contraseña);
+        Passw1.setVisible(false);
+        Text2.setVisible(true);
+
+
+
+    }
 }
